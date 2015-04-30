06a6941c2a60da75cef55935eae0dae6f1732fe8
Jason Dillaman <dillaman@redhat.com>
2015-04-30 13:51:03 -0400

    librbd: give locks unique names to prevent false lockdep failures
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>
    (cherry picked from commit c474ee42b87975c04eeb2b40b976deb5a5e2d518)

--- src/librbd/AioCompletion.h
+++ src/librbd/AioCompletion.h
@@ -67,7 +67,7 @@ namespace librbd {
     xlist<AioCompletion*>::item m_xlist_item;
     bool event_notify;
 
-    AioCompletion() : lock("AioCompletion::lock", true),
+    AioCompletion() : lock(unique_lock_name("AioCompletion::lock", this), true),
 		      done(false), rval(0), complete_cb(NULL),
 		      complete_arg(NULL), rbd_comp(NULL),
 		      pending_count(0), blockers(1),
