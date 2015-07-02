117596c2745f775ee83ef9314d2fb2f3f96d2ba9
Guang G Yang <yguang@renownedground.corp.gq1.yahoo.com>
2015-07-02 05:29:47 +0000

    osd: pg_interval_t::check_new_interval should not rely on pool.min_size to determine if the PG was active
    If the pool's min_size is set improperly, during peering, pg_interval_t::check_new_interval
    might wrongly determine the PG's state and cause the PG to stuck at down+peering forever
    
    Fixes: #12162
    Signed-off-by: Guang Yang yguang@yahoo-inc.com
    (cherry picked from commit 684927442d81ea08f95878a8af69d08d3a14d973)
    
    Conflicts:
    	src/osd/PG.cc
               because PG::start_peering_interval has an assert
               that is not found in hammer in the context
    	src/test/osd/types.cc
               because include/stringify.h is not included by
               types.cc in hammer

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -695,6 +695,8 @@ void PG::generate_past_intervals()
       pgid = pgid.get_ancestor(last_map->get_pg_num(pgid.pool()));
     cur_map->pg_to_up_acting_osds(pgid, &up, &up_primary, &acting, &primary);
 
+    boost::scoped_ptr<IsPGRecoverablePredicate> recoverable(
+      get_is_recoverable_predicate());
     std::stringstream debug;
     bool new_interval = pg_interval_t::check_new_interval(
       old_primary,
@@ -710,6 +712,7 @@ void PG::generate_past_intervals()
       cur_map,
       last_map,
       pgid,
+      recoverable.get(),
       &past_intervals,
       &debug);
     if (new_interval) {
@@ -4760,6 +4763,8 @@ void PG::start_peering_interval(
     info.history.same_interval_since = osdmap->get_epoch();
   } else {
     std::stringstream debug;
+    boost::scoped_ptr<IsPGRecoverablePredicate> recoverable(
+      get_is_recoverable_predicate());
     bool new_interval = pg_interval_t::check_new_interval(
       old_acting_primary.osd,
       new_acting_primary,
@@ -4772,6 +4777,7 @@ void PG::start_peering_interval(
       osdmap,
       lastmap,
       info.pgid.pgid,
+      recoverable.get(),
       &past_intervals,
       &debug);
     dout(10) << __func__ << ": check_new_interval output: "
