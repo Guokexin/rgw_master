e11609ca928299400195e8a871bebb4bbb7a9768
Xinze Chi <xinze@xsky.com>
2016-03-31 10:32:25 +0800

    mscache: add commit 1a9e83c51ea6166f7268c65688008c69f6f7cc69
    Signed-off-by: Xinze Chi <xinze@xsky.com>

--- src/include/mscache_expt.h
+++ src/include/mscache_expt.h
@@ -13,12 +13,22 @@
 #include <tr1/memory>
 #include "os/FDCache.h"
 
-#define MSCACHE_WRITE_API_SYNC  0x1a
-#define MSCACHE_WRITE_API_ASYNC  0x1b
+enum {
+    MSCACHE_WRITE_API_SYNC = 0xb1,
+    MSCACHE_WRITE_API_ASYNC,    
+};
+
+enum {
+    MSCACHE_WRITE_HINT_DATA = 0xa0,
+    MSCACHE_WRITE_HINT_METADATA,
+    MSCACHE_WRITE_HINT_TRUNCATE,
+};
+
+
 
 extern int mscache_read(FDRef &fdref, const char* obj_info, void* buff, size_t length, off_t offset, int pref_flag);
 
-extern int mscache_write(FDRef &fdref, const char* obj_info, const void* buff, size_t length, off_t offset, void* arg, int sync_flag);
+extern int mscache_write(FDRef &fdref, const char* obj_info, const void* buff, size_t length, off_t offset, void* arg, int async_flag, int write_hint, int truncate_length);
 
 extern int mscache_modules_init(void);
 
