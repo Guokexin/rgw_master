ceph/ceph

rgw:add --reset-regions for regionmap update
Fixes: #12964

Signed-off-by: Weijun Duan <duanweijun@h3c.com>

src/rgw/rgw_admin.cc
@@ -165,6 +165,7 @@ void _usage()
   cout << "   --categories=<list>       comma separated list of categories, used in usage show\n";
   cout << "   --caps=<caps>             list of caps (e.g., \"usage=read, write; user=read\"\n";
   cout << "   --yes-i-really-mean-it    required for certain operations\n";
+  cout << "   --reset-regions           reset regionmap when regionmap update";
   cout << "\n";
   cout << "<date> := \"YYYY-MM-DD[ hh:mm:ss]\"\n";
   cout << "\nQuota options:\n";
@@ -1142,6 +1143,7 @@ int main(int argc, char **argv)
   int include_all = false;
 
   int sync_stats = false;
+  int reset_regions = false;
 
   uint64_t min_rewrite_size = 4 * 1024 * 1024;
   uint64_t max_rewrite_size = ULLONG_MAX;
@@ -1313,6 +1315,8 @@ int main(int argc, char **argv)
     } else if (ceph_argparse_binary_flag(args, i, &sync_stats, NULL, "--sync-stats", (char*)NULL)) {
      // do nothing
     } else if (ceph_argparse_binary_flag(args, i, &include_all, NULL, "--include-all", (char*)NULL)) {
+     // do nothing
+    } else if (ceph_argparse_binary_flag(args, i, &reset_regions, NULL, "--reset-regions", (char*)NULL)) {
      // do nothing
     } else if (ceph_argparse_witharg(args, i, &val, "--caps", (char*)NULL)) {
       caps = val;
@@ -1550,6 +1554,10 @@ int main(int argc, char **argv)
       if (ret < 0) {
         cerr << "failed to list regions: " << cpp_strerror(-ret) << std::endl;
 	return -ret;
+      }
+
+      if (reset_regions) {
+        regionmap.regions.clear();
       }
 
       for (list<string>::iterator iter = regions.begin(); iter != regions.end(); ++iter) {

Guokexin/rgw_master

(cherry picked from commit 95685c19d6f1eab50b903e61273b5351bedc2980)
Conflicts:
	src/rgw/rgw_admin.cc: usage is on cerr in hammer, not cout
        src/test/cli/radosgw-admin/help.t: remove extra empty line

src/rgw/rgw_admin.cc
@@ -166,6 +166,7 @@ void _usage()
   cerr << "   --categories=<list>       comma separated list of categories, used in usage show\n";
   cerr << "   --caps=<caps>             list of caps (e.g., \"usage=read, write; user=read\"\n";
   cerr << "   --yes-i-really-mean-it    required for certain operations\n";
+  cerr << "   --reset-regions           reset regionmap when regionmap update";
   cerr << "\n";
   cerr << "<date> := \"YYYY-MM-DD[ hh:mm:ss]\"\n";
   cerr << "\nQuota options:\n";
@@ -1144,6 +1145,7 @@ int main(int argc, char **argv)
   int include_all = false;
 
   int sync_stats = false;
+  int reset_regions = false;
 
   uint64_t min_rewrite_size = 4 * 1024 * 1024;
   uint64_t max_rewrite_size = ULLONG_MAX;
@@ -1315,6 +1317,8 @@ int main(int argc, char **argv)
     } else if (ceph_argparse_binary_flag(args, i, &sync_stats, NULL, "--sync-stats", (char*)NULL)) {
      // do nothing
     } else if (ceph_argparse_binary_flag(args, i, &include_all, NULL, "--include-all", (char*)NULL)) {
+     // do nothing
+    } else if (ceph_argparse_binary_flag(args, i, &reset_regions, NULL, "--reset-regions", (char*)NULL)) {
      // do nothing
     } else if (ceph_argparse_witharg(args, i, &val, "--caps", (char*)NULL)) {
       caps = val;
@@ -1552,6 +1556,10 @@ int main(int argc, char **argv)
       if (ret < 0) {
         cerr << "failed to list regions: " << cpp_strerror(-ret) << std::endl;
 	return -ret;
+      }
+
+      if (reset_regions) {
+        regionmap.regions.clear();
       }
 
       for (list<string>::iterator iter = regions.begin(); iter != regions.end(); ++iter) {