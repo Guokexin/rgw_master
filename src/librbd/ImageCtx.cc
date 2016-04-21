varadakari/ceph

tests: fix possible deadlock in librbd ImageWatcher tests
Signed-off-by: Jason Dillaman <dillaman@redhat.com>

src/librbd/ImageCtx.cc
@@ -689,7 +689,7 @@ public:
 
   void ImageCtx::invalidate_cache(Context *on_finish) {
     if (object_cacher == NULL) {
-      on_finish->complete(0);
+      work_queue->queue(on_finish, 0);
       return;
     }
 

Guokexin/rgw_master

(cherry picked from commit 742a85d667b26b3490d96270b5c500b08f2a5283)

src/librbd/ImageCtx.cc
@@ -745,7 +745,7 @@ public:
 
   void ImageCtx::invalidate_cache(Context *on_finish) {
     if (object_cacher == NULL) {
-      on_finish->complete(0);
+      op_work_queue->queue(on_finish, 0);
       return;
     }
 