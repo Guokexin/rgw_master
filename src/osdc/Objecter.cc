lihaijing/ceph

librados: fix narrow race with tid return value assignment
The completion should be done by the time op_submit returns.

Signed-off-by: Sage Weil <sage@redhat.com>

src/osdc/Objecter.cc
@@ -2125,12 +2125,13 @@ void Objecter::resend_mon_ops()
 
 // read | write ---------------------------
 
-ceph_tid_t Objecter::op_submit(Op *op, int *ctx_budget)
+void Objecter::op_submit(Op *op, ceph_tid_t *ptid, int *ctx_budget)
 {
   shunique_lock rl(rwlock, ceph::acquire_shared);
   ceph_tid_t tid = 0;
-  _op_submit_with_budget(op, rl, &tid, ctx_budget);
-  return tid;
+  if (!ptid)
+    ptid = &tid;
+  _op_submit_with_budget(op, rl, ptid, ctx_budget);
 }
 
 void Objecter::_op_submit_with_budget(Op *op, shunique_lock& sul,

Guokexin/rgw_master

src/osdc/Objecter.cc
@@ -1974,12 +1974,14 @@ public:
   }
 };
 
-ceph_tid_t Objecter::op_submit(Op *op, int *ctx_budget)
+void Objecter::op_submit(Op *op, ceph_tid_t *ptid, int *ctx_budget)
 {
   RWLock::RLocker rl(rwlock);
   RWLock::Context lc(rwlock, RWLock::Context::TakenForRead);
   ceph_tid_t tid = 0;
-  return _op_submit_with_budget(op, lc, &tid, ctx_budget);
+  if (!ptid)
+    ptid = &tid;
+  _op_submit_with_budget(op, lc, ptid, ctx_budget);
 }
 
 ceph_tid_t Objecter::_op_submit_with_budget(Op *op, RWLock::Context& lc, ceph_tid_t *ptid, int *ctx_budget)