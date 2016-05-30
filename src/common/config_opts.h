10354709ebef9888d4ef710aaa0f2336b31fe888
Zhiqiang Wang <zhiqiang@xsky.com>
2016-05-30 09:22:55 -0400

    mon: adjust lease and election related timeout values
    Signed-off-by: Zhiqiang Wang <zhiqiang@xsky.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -204,14 +204,14 @@ OPTION(mon_osd_allow_primary_affinity, OPT_BOOL, false)  // allow primary_affini
 OPTION(mon_osd_prime_pg_temp, OPT_BOOL, true)  // prime osdmap with pg mapping changes
 OPTION(mon_osd_prime_pg_temp_max_time, OPT_FLOAT, .5)  // max time to spend priming
 OPTION(mon_stat_smooth_intervals, OPT_INT, 2)  // smooth stats over last N PGMap maps
-OPTION(mon_election_timeout, OPT_FLOAT, 5)  // on election proposer, max waiting time for all ACKs
-OPTION(mon_lease, OPT_FLOAT, 5)       // lease interval
-OPTION(mon_lease_renew_interval, OPT_FLOAT, 3) // on leader, to renew the lease
-OPTION(mon_lease_ack_timeout, OPT_FLOAT, 10.0) // on leader, if lease isn't acked by all peons
+OPTION(mon_election_timeout, OPT_FLOAT, 2)  // on election proposer, max waiting time for all ACKs
+OPTION(mon_lease, OPT_FLOAT, 3)       // lease interval
+OPTION(mon_lease_renew_interval, OPT_FLOAT, 1.5) // on leader, to renew the lease
+OPTION(mon_lease_ack_timeout, OPT_FLOAT, 5.0) // on leader, if lease isn't acked by all peons
 OPTION(mon_clock_drift_allowed, OPT_FLOAT, .050) // allowed clock drift between monitors
 OPTION(mon_clock_drift_warn_backoff, OPT_FLOAT, 5) // exponential backoff for clock drift warnings
 OPTION(mon_timecheck_interval, OPT_FLOAT, 300.0) // on leader, timecheck (clock drift check) interval (seconds)
-OPTION(mon_accept_timeout, OPT_FLOAT, 10.0)    // on leader, if paxos update isn't accepted
+OPTION(mon_accept_timeout, OPT_FLOAT, 5.0)    // on leader, if paxos update isn't accepted
 OPTION(mon_pg_create_interval, OPT_FLOAT, 30.0) // no more than every 30s
 OPTION(mon_pg_stuck_threshold, OPT_INT, 300) // number of seconds after which pgs can be considered inactive, unclean, or stale (see doc/control.rst under dump_stuck for more info)
 OPTION(mon_pg_warn_min_per_osd, OPT_INT, 0)  // min # pgs per (in) osd before we warn the admin
