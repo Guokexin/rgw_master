345595c02d3d8bea2d6ccd06122be42232b96649
Haomai Wang <haomai@xsky.com>
2016-05-30 12:28:18 +0800

    rbd_test: disable exclusive lock and objectmap tests
    Signed-off-by: Haomai Wang <haomai@xsky.com>

--- src/test/run-rbd-valgrind-unit-tests.sh
+++ src/test/run-rbd-valgrind-unit-tests.sh
@@ -6,6 +6,6 @@ CEPH_SRC=$(pwd)
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CEPH_SRC/.libs"
 PATH="$CEPH_SRC:$PATH"
 
-RBD_FEATURES=13 valgrind --tool=memcheck --leak-check=full --suppressions=valgrind.supp unittest_librbd
+RBD_FEATURES=1 valgrind --tool=memcheck --leak-check=full --suppressions=valgrind.supp unittest_librbd
 
 echo OK
