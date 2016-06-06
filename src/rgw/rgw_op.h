rzarzynski/ceph

rgw: add basic support for X-Delete-At header of Swift API.
Fixes: #4099
Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

src/rgw/rgw_op.h
@@ -176,6 +176,7 @@ public:
   virtual const string name() { return "get_obj"; }
   virtual RGWOpType get_type() { return RGW_OP_GET_OBJ; }
   virtual uint32_t op_mask() { return RGW_OP_TYPE_READ; }
+  virtual bool need_object_expiration() { return false; }
 };
 
 #define RGW_LIST_BUCKETS_LIMIT_MAX 10000
@@ -515,6 +516,7 @@ protected:
   RGWAccessControlPolicy policy;
   RGWCORSConfiguration cors_config;
   string placement_rule;
+  string delete_at;
 
 public:
   RGWPutMetadata() {

Guokexin/rgw_master

src/rgw/rgw_op.h
@@ -176,6 +176,7 @@ public:
   virtual const string name() { return "get_obj"; }
   virtual RGWOpType get_type() { return RGW_OP_GET_OBJ; }
   virtual uint32_t op_mask() { return RGW_OP_TYPE_READ; }
+  virtual bool need_object_expiration() { return false; }
 };
 
 #define RGW_LIST_BUCKETS_LIMIT_MAX 10000
@@ -533,7 +534,11 @@ public:
 class RGWSetTempUrl : public RGWOp {
 protected:
   int ret;
+
   map<int, string> temp_url_keys;
+
+  time_t delete_at;
+
 public:
   RGWSetTempUrl() : ret(0) {}
 